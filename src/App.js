527ec7b792bd36d16bc45b3650b166b1b6c11149
yujhenchen <yujhen.chen93@gmail.com>
2023-02-20 11:50:40 +0800

    add class component, simplify using component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import logo from "./logo.svg";
+// import logo from "./logo.svg";
 import "./App.css";
 import Hello from "./components/Hello";
+import Message from "./components/Message";
 
 function App() {
   return (
@@ -19,7 +20,8 @@ function App() {
           Learn React
         </a>
       </header> */}
-      <Hello></Hello>
+      <Hello />
+      <Message />
     </div>
   );
 }
